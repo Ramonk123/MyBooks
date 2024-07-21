0466991c53741aa1d3fe736fea795a3973225fbb
RamonK <ramonkrijnen10@outlook.com>
2024-07-21 12:23:31 +0200

    Database setup

--- MyBooks/Libraries/Data/MyBooksDbContext.cs
+++ MyBooks/Libraries/Data/MyBooksDbContext.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace WebApplication1.Libraries.Data;
 
-public class MyBooksDbContext
+public class MyBooksDbContext : DbContext
 {
-    
+    public MyBooksDbContext(DbContextOptions<MyBooksDbContext> options) : base(options)
+    {
+        // A4Cj!0:[wpck48jmc15418cn2xcKAH8SD
+    }
 }
--- MyBooks/Startup.cs
+++ MyBooks/Startup.cs
@@ -1,6 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Libraries.Data;
+
 namespace WebApplication1;
 
 public class Startup
 {
-    
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration, IHostEnvironment env)
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json");
+
+        if (env.IsDevelopment())
+        {
+            builder.AddUserSecrets<Startup>();
+        }
+
+        builder.AddEnvironmentVariables();
+
+        Configuration = builder.Build();
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<MyBooksDbContext>(options =>
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+    }
 }
