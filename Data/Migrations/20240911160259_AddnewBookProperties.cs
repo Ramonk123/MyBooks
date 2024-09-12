01b4102711119cce930b738b12662aee4254e338
RamonK <ramonkrijnen10@outlook.com>
2024-09-12 14:14:53 +0200

    Add seeder

---
+++ Data/Migrations/20240911160259_AddnewBookProperties.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddnewBookProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "AverageRating",
+                table: "Books",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReleaseYear",
+                table: "Books",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageRating",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseYear",
+                table: "Books");
+        }
+    }
+}
